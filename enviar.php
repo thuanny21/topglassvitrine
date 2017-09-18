21e5b440b11d3728f66ebda6142423724b3ba280
Thuanny <thucatm@gmail.com>
2017-09-18 09:51:08 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,41 @@
+<?php
+
+// Aqui ficam os dados do seu e-mail e da autoresposta!!!
+
+$assunto = "Contato Site"; //Assunto do e-mail q vai chegar na sua caixa de mensagem
+$mail = "thucatm@gmail.com"; //E-mail que você gostaria de receber os resultados dos formmail's
+$assunto_auto = "$nome, Sua Mensagem Foi Recebida Com Sucesso!";//Assunto da Auto Resposta
+$website =  "http://www.topglassvitrine.com/";//Url do Website
+$nome_webmaster = "Top Glass Vitrine";//Nome do Webmaster do site
+$mensagem_auto = "Obrigado por entrar em contato conosco $nome!\nO mais breve possivel estaremos respondendo sua mensagem!!!\n\n  - $nome_webmaster";
+$assunto_auto = "Recebemos sua mensagem";
+
+// Aqui ficam os dados do formulário que serão enviados!!!
+
+$ip = getenv("REMOTE_ADDR");
+$navegador = $_SERVER['HTTP_USER_AGENT']; 
+$nome = $_POST["nome"];//Campo Nome do Formulário
+$email = $_POST["email"];//Campo E-mail do Formulário
+$telefone = $_POST["telefone"];//Campo Contato do Formulário
+$cidade = $_POST["cidade"];//Campo Cidade do Formulário
+$comentario = $_POST["comentario"];//Campo Mensagem do Formulário
+
+$mensagem = "Formulário enviado por $nome no Website $website:\n\n";//Inicio da Mensagem enviada! 
+$mensagem .= "Nome: $nome\n";//Nome do Contato
+$mensagem .= "E-mail: $email\n";//Nome do Contato
+$mensagem .= "Telefone: $telefone\n";//Telefone do Contato
+$mensagem .= "Cidade: $cidade\n";//Cidade do Contato
+$mensagem .= "Comentario: $comentario\n";//Comentario do Contato
+$mensagem .= "IP do visitante: $ip\n";
+
+
+
+//não modifique esta linha, pois é ela que envia a mensagem!!!
+@mail($mail, $assunto, $mensagem, "From: $email");
+
+//não modifique esta linha, pois é ela que envia a auto_resposta!!!
+@mail($email, $assunto_auto, $mensagem_auto, "From: $mail");
+
+header("Location:ok.php");
+
+?>
---
+++ ok.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Top Glass Vitrine</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+
+
+   <link rel="sortcut icon" href="imagem/topglassvitrine.gif" type="image/gif"/>
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+      .login-page
+{
+    background-image: url('../imagem/topglassvitrine6.jpeg');
+    background-repeat: no-repeat;
+    background-size:100%;
+    bottom: 0;
+    color: black;
+    left: 0;
+    overflow: auto;
+    padding: 3em;
+    position: absolute;
+    right: 0;
+    text-align: center;
+    top: 0;
+
+}
+.login-page h1
+{
+    font-weight: 300;
+}
+.login-page h1 small
+{
+    color: gray;
+}
+.login-page .form-group
+{
+    padding: 8px 0;
+}
+.login-page .form-content
+{
+    padding: 40px 0;
+}
+    </style>
+  </head>
+
+<body>
+
+<div class=login-page>
+
+   <div class=row>
+      <div class="col-md-4 col-lg-4 col-md-offset-4 col-lg-offset-4">
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+         <h1>Sua Mensagem Foi Enviada Com Sucesso!!!</h1>
+         <h2>O mais breve possivel estaremos respondendo sua mensagem</h2>
+
+         <a href="http://topglassvitrine.com" class="btn"> Voltar </a>
+        
+      </div>
+   </div>
+</div>
+</body>
+<html>
+
+
